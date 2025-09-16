5fca5829b19f9cc6cdb257080368c2cb86808163
Pett97 <henriquepeterson138@gmail.com>
2025-09-16 09:17:09 -0300

    button-component

--- mapas-ods-front/src/app/app.ts
+++ mapas-ods-front/src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Button } from './shared/components/button/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Button],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('mapas-ods-front');
+
+  onButtonClick(){
+    console.log("12313");
+  }
 }
