1fa0e2ef4c05cb9d0cd0635fb87927820255cda7
Pett97 <henriquepeterson138@gmail.com>
2025-10-16 00:30:59 -0300

    ajuste

--- mapas-ods-front/src/app/app.routes.ts
+++ mapas-ods-front/src/app/app.routes.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-import { TelaLogin } from './pages/tela-login/tela-login';
-import { Home } from './pages/home/home'
+
 import { ContasSanepar } from './pages/contaSanepar/contas-sanepar/contas-sanepar';
 import { FormContaSanepar } from './pages/contaSanepar/form-conta-sanepar/form-conta-sanepar';
-import { NavbarAdm } from './pages/navbar-adm/navbar-adm';
 import { Eventos } from './pages/eventos/eventos';
 import { FormEventos } from './pages/eventos/form-eventos/form-eventos';
+import { Home } from './pages/home/home';
+import { NavbarAdm } from './pages/navbar-adm/navbar-adm';
+import { TelaLogin } from './pages/tela-login/tela-login';
 
 export const routes: Routes = [
    {
@@ -28,26 +28,26 @@ export const routes: Routes = [
    {
       path: 'atualizar-leitura-sanepar/:id',
       title: "Atualizar Leitura Sanpear",
-      component:FormContaSanepar
-   },{
-      path:"eventos",
-      title:"Eventos",
-      component:Eventos
+      component: FormContaSanepar
+   }, {
+      path: "eventos",
+      title: "Eventos",
+      component: Eventos
    },
-  {
+   {
       path: 'login',
       title: "Tela de Login",
       component: TelaLogin
    },
    {
-      path:"gerenciar-evento/:id",
-      title:"Gerenciar Evento",
-      component:FormEventos
+      path: "gerenciar-evento/:id",
+      title: "Gerenciar Evento",
+      component: FormEventos
    },
    {
-      path: 'xxxxxx',
+      path: '',
       title: "Home Mapa ODS",
-      component: Eventos,
+      component: Home,
       pathMatch: "full"
    }
 ];
--- mapas-ods-front/src/app/app.ts
+++ mapas-ods-front/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarAdm } from './pages/navbar-adm/navbar-adm';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarAdm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
